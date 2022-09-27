04a48dc9bcde325905f2f47487090d91e344bcbc
Mohan Varma Uppalapati <mohan@vcollab.com>
2022-09-27 15:00:56 +0530

    Making email non-unique

--- users_mgt.py
+++ users_mgt.py
@@ -17,7 +17,7 @@ class User(db.Model):
     }
     id = db.Column(db.Integer, primary_key=True, nullable=False, autoincrement=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
-    email = db.Column(db.String(50), unique=True, nullable=False)
+    email = db.Column(db.String(50), nullable=False)
     password = db.Column(db.String(100), nullable=False)
     admin = db.Column(db.Boolean, nullable=False, default=False)
 
